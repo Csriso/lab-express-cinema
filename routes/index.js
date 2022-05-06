064b09d0a063afd7f1fe5753dadfca99f128f484
unknown <cesyrub@gmail.com>
2022-05-06 23:01:50 +0200

    Lab finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  const moviesModel = require("../models/Movie.model");
+
+  moviesModel
+    .find()
+    .then((response) => {
+      res.render("movies.hbs", {
+        movies: response,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/movie/:id", (req, res, next) => {
+  const moviesModel = require("../models/Movie.model");
+  const { id } = req.params;
+  moviesModel
+    .find({ _id: id })
+    .then((response) => {
+      res.render("movie.hbs", {
+        movie: response[0],
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
